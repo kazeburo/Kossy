49a9940a6e0221edba6f8d77c4dd67fea9b566f9
Masahiro Nagano <kazeburo@gmail.com>
2011-11-11 10:35:26 +0900

    initial commit

---
+++ lib/Kossy.pm
@@ -0,0 +1,32 @@
+package Kossy;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Kossy -
+
+=head1 SYNOPSIS
+
+  use Kossy;
+
+=head1 DESCRIPTION
+
+Kossy is
+
+=head1 AUTHOR
+
+Masahiro Nagano E<lt>kazeburo {at} gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
